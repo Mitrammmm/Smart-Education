3d4e0794b3dd9a92f0871e77d75e20fb7ba276c4
Mitrammmm <mitrasinghimp@gmail.com>
2024-04-25 12:05:20 +0530

    Filter

--- db/dbcon.js
+++ db/dbcon.js
@@ -1,11 +1,11 @@
 
 const mongoose = require('mongoose');
 
-const localUrl = 'mongodb://127.0.0.1:27017/SmartEducation';
-// const liveUrl = 'mongodb+srv://collablab2243:collabLabs@cluster0.40vgfba.mongodb.net/CollabLab?retryWrites=true&w=majority&appName=Cluster0';
+// const localUrl = 'mongodb://127.0.0.1:27017/SmartEducation';
+const liveUrl = 'mongodb+srv://collablab2243:collabLabs@cluster0.40vgfba.mongodb.net/CollabLab?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDb = async () => {
-    return mongoose.connect(localUrl)
+    return mongoose.connect(liveUrl)
     .then(() => {
         console.log("Connected to MongoDB");
     })
